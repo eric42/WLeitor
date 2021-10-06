fb7a0896fc808d7dbedb191d33de7c7f5cd9e75b
Eric <eric_eumesmo@yahoo.com.br>
2021-10-06 14:41:32 -0300

    Adicionar arquivos de projeto.

---
+++ WLeitor/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace WLeitor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lstArquivos = new System.Windows.Forms.ListBox();
+            this.txtArquivo = new System.Windows.Forms.TextBox();
+            this.btnSelecionar = new System.Windows.Forms.Button();
+            this.btnGerar = new System.Windows.Forms.Button();
+            this.fbd1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lstArquivos);
+            this.groupBox1.Controls.Add(this.txtArquivo);
+            this.groupBox1.Location = new System.Drawing.Point(13, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(443, 307);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Notas Fiscais";
+            // 
+            // lstArquivos
+            // 
+            this.lstArquivos.FormattingEnabled = true;
+            this.lstArquivos.Location = new System.Drawing.Point(6, 19);
+            this.lstArquivos.Name = "lstArquivos";
+            this.lstArquivos.Size = new System.Drawing.Size(431, 277);
+            this.lstArquivos.TabIndex = 0;
+            // 
+            // txtArquivo
+            // 
+            this.txtArquivo.Location = new System.Drawing.Point(153, 195);
+            this.txtArquivo.Name = "txtArquivo";
+            this.txtArquivo.Size = new System.Drawing.Size(100, 20);
+            this.txtArquivo.TabIndex = 0;
+            this.txtArquivo.Visible = false;
+            // 
+            // btnSelecionar
+            // 
+            this.btnSelecionar.Location = new System.Drawing.Point(69, 346);
+            this.btnSelecionar.Name = "btnSelecionar";
+            this.btnSelecionar.Size = new System.Drawing.Size(138, 23);
+            this.btnSelecionar.TabIndex = 1;
+            this.btnSelecionar.Text = "Selecionar";
+            this.btnSelecionar.UseVisualStyleBackColor = true;
+            this.btnSelecionar.Click += new System.EventHandler(this.btnSelecionar_Click);
+            // 
+            // btnGerar
+            // 
+            this.btnGerar.Location = new System.Drawing.Point(224, 346);
+            this.btnGerar.Name = "btnGerar";
+            this.btnGerar.Size = new System.Drawing.Size(151, 23);
+            this.btnGerar.TabIndex = 2;
+            this.btnGerar.Text = "Gerar Relatorio";
+            this.btnGerar.UseVisualStyleBackColor = true;
+            this.btnGerar.Click += new System.EventHandler(this.btnGerar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(468, 387);
+            this.Controls.Add(this.btnGerar);
+            this.Controls.Add(this.btnSelecionar);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Sonda Leitor Whirlpool XML";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ListBox lstArquivos;
+        private System.Windows.Forms.Button btnSelecionar;
+        private System.Windows.Forms.Button btnGerar;
+        private System.Windows.Forms.FolderBrowserDialog fbd1;
+        private System.Windows.Forms.TextBox txtArquivo;
+    }
+}
+
---
+++ WLeitor/Form1.cs
@@ -0,0 +1,166 @@
+﻿using OfficeOpenXml;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using LicenseContext = OfficeOpenXml.LicenseContext;
+
+namespace WLeitor
+{
+    public partial class Form1 : Form
+    {
+
+        public List<string> listaArquivo = new List<string>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSelecionar_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            DialogResult result = fbd.ShowDialog();
+
+            //Marca o diretório a ser listado
+            //DirectoryInfo diretorio = new DirectoryInfo(txtArquivo.Text);
+            //Executa função GetFile(Lista os arquivos desejados de acordo com o parametro)
+            
+            var Arquivos = Directory.EnumerateFiles(fbd.SelectedPath, "*.xml", SearchOption.AllDirectories);
+
+            foreach (var y in Arquivos)
+            {
+                lstArquivos.Items.Add(Path.GetFileName(y));
+                listaArquivo.Add(y);
+            }
+
+        }
+
+        private void btnGerar_Click(object sender, EventArgs e)
+        {
+            if (lstArquivos.Items.Count > 0)
+            {
+                List<string> lista = new List<string>();
+
+                foreach (string x in listaArquivo)
+                {
+                    lista.Add(x);
+                }
+
+                XmlNamespaceManager nsmgr = new XmlNamespaceManager(new NameTable());
+                nsmgr.AddNamespace("n", "http://www.portalfiscal.inf.br/nfe");
+
+                XmlDocument oXML = new XmlDocument();
+
+                XmlNode root;
+
+                XmlNodeList cnpj, nf, codProd, desc, unidade, quantidade, vlr;
+                FolderBrowserDialog fbd = new FolderBrowserDialog();
+
+                ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+                int counter = 1, i = 1, l = 2, k = 2, h = 2, g = 2, f = 2, a = 1;
+
+
+                using (var excelPackage = new ExcelPackage())
+                {
+                    excelPackage.Workbook.Properties.Title = "Relatório";
+                    var sheet = excelPackage.Workbook.Worksheets.Add("Relatório");
+                    string caminho;
+
+                    string[] titulos = new string[] {"NUM_NF", "CNPJ_EMITENTE", "SEQ_ITEM", "COD_PRODUTO",
+                    "DESCRICAO", "UNIDADE_MEDIDA", "QUANTIDADE", "VLR_BRUTO"};
+
+                    foreach (var titulo in titulos)
+                    {
+                        sheet.Cells[1, i++].Value = titulo;
+                    }
+
+                    i = 2;
+
+                    foreach (var y in lista)
+                    {
+                        oXML.Load(y);
+                        root = oXML.DocumentElement;
+                        cnpj = root.SelectNodes("//n:CNPJ", nsmgr);
+                        nf = root.SelectNodes("//n:nNF", nsmgr);
+                        codProd = root.SelectNodes("//n:cProd", nsmgr);
+                        desc = root.SelectNodes("//n:xProd", nsmgr);
+                        unidade = root.SelectNodes("//n:uCom", nsmgr);
+                        quantidade = root.SelectNodes("//n:qCom", nsmgr);
+                        vlr = root.SelectNodes("//n:vProd", nsmgr);
+
+                        foreach (XmlNode oNo in cnpj)
+                        {
+                            foreach (XmlNode oNo1 in nf)
+                            {
+                                for (int z = 0; z < codProd.Count; z++)
+                                {
+                                    sheet.Cells[i, 1].Value = oNo1.ChildNodes.Item(0).InnerText;
+                                    sheet.Cells[i, 2].Value = oNo.ChildNodes.Item(0).InnerText;
+                                    i++;
+                                }
+                            }
+
+                            foreach (XmlNode oNo1 in codProd)
+                            {
+                                sheet.Cells[l, 4].Value = oNo1.ChildNodes.Item(0).InnerText;
+                                sheet.Cells[l, 3].Value = counter;
+                                counter++;
+                                l++;
+                            }
+
+                            foreach (XmlNode oNo1 in desc)
+                            {
+                                sheet.Cells[k, 5].Value = oNo1.ChildNodes.Item(0).InnerText;
+                                k++;
+                            }
+
+
+                            foreach (XmlNode oNo1 in unidade)
+                            {
+                                sheet.Cells[f, 6].Value = oNo1.ChildNodes.Item(0).InnerText;
+                                f++;
+                            }
+
+                            foreach (XmlNode oNo1 in quantidade)
+                            {
+                                sheet.Cells[h, 7].Value = oNo1.ChildNodes.Item(0).InnerText;
+                                h++;
+                            }
+
+                            foreach (XmlNode oNo1 in vlr)
+                            {
+                                if (a < vlr.Count)
+                                {
+                                    sheet.Cells[g, 8].Value = oNo1.ChildNodes.Item(0).InnerText;
+                                    g++;
+                                    a++;
+                                }
+                            }
+                            a = 1;
+                            counter = 1;
+                            break;
+                        }
+                    }
+
+                    caminho = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+                    string path = caminho + @"\Relatorio.xlsx";
+                    File.WriteAllBytes(path, excelPackage.GetAsByteArray());
+                    MessageBox.Show("Concluído. Verifique em " + Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments));
+                }
+            }
+            else
+            {
+                MessageBox.Show("Não é possivel gerar o relatório sem selecionar ao menos 1 XML!", "Aviso", MessageBoxButtons.OK);
+            }
+
+        }
+    }
+}
